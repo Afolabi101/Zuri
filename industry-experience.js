2cc19c842dd73a05468a1d84060c258bd3ddfa4e
Tech-Titan <101532815+Afolabi101@users.noreply.github.com>
2022-08-16 09:09:24 +0100

    Update industry-experience.js

--- industry-experience.js
+++ industry-experience.js
@@ -25,11 +25,11 @@ cross.addEventListener("click", rotate);
 //     cross.classList.toggle("rotate");
 // }
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
